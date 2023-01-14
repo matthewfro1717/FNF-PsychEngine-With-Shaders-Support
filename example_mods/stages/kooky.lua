263898730983753348a7e073e0d183e4727592c5
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-01-14 09:45:47 -0500

    Add files via upload

---
+++ example_mods/stages/kooky.lua
@@ -0,0 +1,44 @@
+--made with Super_Hugo's Stage Editor
+
+function onCreate()
+
+	makeLuaSprite('obj2', 'bg', 161, 266)
+
+	setObjectOrder('obj2', 1)
+	scaleObject('obj2', 2.1, 2.1)
+
+	addLuaSprite('obj2', true)
+
+
+	makeLuaSprite('obj4', 'white', 113, -214)
+
+	setObjectOrder('obj4', 2)
+	scaleObject('obj4', 1.7, 1.7)
+
+	addLuaSprite('obj4', true)
+
+
+	makeLuaSprite('obj5', 'white', 1252, 286)
+
+	setObjectOrder('obj5', 2)
+	scaleObject('obj5', 1.4, 1.4)
+
+	addLuaSprite('obj5', true)
+
+
+	makeLuaSprite('obj3', 'white', 1225, 592)
+
+	setObjectOrder('obj3', 4)
+	scaleObject('obj3', 1.5, 1.5)
+
+	addLuaSprite('obj3', true)
+
+
+	makeLuaSprite('obj1', 'unknown', -251, -569)
+
+	setObjectOrder('obj1', 5)
+	scaleObject('obj1', 1.7, 1.7)
+
+	addLuaSprite('obj1', true)
+
+end

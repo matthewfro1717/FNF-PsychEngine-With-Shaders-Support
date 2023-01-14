761c18e2f1403bc3f926888179fa2c2ce9901afb
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-01-14 11:28:47 -0500

    Create hide-opponent-notes.lua

---
+++ example_mods/data/kooky/hide-opponent-notes.lua
@@ -0,0 +1,12 @@
+now try:
+function onCreatePost()
+
+for i = 0, getProperty('opponentStrums.length')-1 do
+
+setPropertyFromGroup('opponentStrums',i,'visible',false)
+
+setPropertyFromGroup('opponentStrums',i,'y',130)
+
+setPropertyFromGroup('opponentStrums',i,'x',-9999)
+
+end
